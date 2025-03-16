5378b74de072c56cb954e8cb5cf953af7978f90b
ldev <ldev@ldev.eu.org>
2025-03-16 12:52:54 +0100

    some fixes

--- run_prod.sh
+++ run_prod.sh
@@ -5,7 +5,7 @@ GIT="git"
 
 $GIT reset --hard || exit 1
 $GIT pull || exit 1
-$DOCKER system prune
+$DOCKER system prune -f
 
 $DOCKER_COMPOSE -f compose.yml -f prod.compose.yml up --detach || exit 1
 echo "done"
