ab4f3d50db7acda1649c7d50ec00392381610acf
ldev <ldev@ldev.eu.org>
2025-09-01 20:21:37 +0200

    added http_req_total metric

--- lmetrics/src/httpmetrics.rs
+++ lmetrics/src/httpmetrics.rs
@@ -4,19 +4,17 @@ use crate::metrics;
 
 metrics! {
 pub counter http_errors_total("Amount of total http errors",
-    [method, status_code]);
-pub counter http_req_total("Amount of total http requests",
-    [method, status_code]);
+    [status_code]);
+pub counter http_req_total("Amount of total http requests");
 }
 
 pub fn http_errors_metrics() -> AdHoc {
-    AdHoc::on_response("response metrics", |req, res| {
+    AdHoc::on_response("response metrics", |_, res| {
         Box::pin(async move {
             let class = res.status().class();
-            let meth = req.method().to_string();
-            http_req_total::inc(&meth, &res.status().code.to_string());
+            http_req_total::inc();
             if class == StatusClass::ClientError || class == StatusClass::ServerError {
-                http_errors_total::inc(&meth, &res.status().code.to_string());
+                http_errors_total::inc(&res.status().code.to_string());
             }
         })
     })
