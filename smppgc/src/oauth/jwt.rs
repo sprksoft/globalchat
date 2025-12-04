0d2aba288c2227be2a86c0497e1d17042955ab63
ldev <ldev@ldev.eu.org>
2025-12-04 19:42:50 +0100

    Switch jwt decoding to URL_SAFE_NO_PAD engine.

--- smppgc/src/oauth/jwt.rs
+++ smppgc/src/oauth/jwt.rs
@@ -14,7 +14,7 @@ pub enum Error {
 
 pub fn decode_payload_insecure<T: for<'de> Deserialize<'de>>(token: &str) -> Result<T, Error> {
     let payload = token.split(".").nth(1).ok_or(Error::Invalid)?;
-    let decoded = base64::engine::general_purpose::STANDARD_NO_PAD.decode(payload)?;
+    let decoded = base64::engine::general_purpose::URL_SAFE_NO_PAD.decode(payload)?;
     let json = serde_json::from_slice::<T>(decoded.as_slice())?;
     Ok(json)
 }
